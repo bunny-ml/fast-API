aea0928d8a5cfb57e0309a1f3f95d32e1dc1e836
bunny-ml <abhishek260204@gmail.com>
2025-04-12 23:01:07 +0530

    commit

--- fastAPI.py
+++ fastAPI.py
@@ -17,7 +17,19 @@ teas: List[Tea] = []
 def read_root():
     return {"Welcome to tea hpuse"}
 
-@app.get("/teas")
+@app.get("/tea")
 def get_teas():
     return teas
 
+@app.post("/teas")
+def add_tea(tea: Tea):
+    teas.append(tea)
+    return tea
+
+@app.put("/teas/{tea_id}")
+def update_tea(tea_id: int , updated_tea: Tea):
+    for index , tea in enumerate(teas):
+        if tea.id == tea_id:
+            teas[index] = update_tea
+            return update_tea
+    return {"error": " tea not found"}
